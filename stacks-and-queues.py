401f9fba7fe2f737b192299d9c38566944c6a0c2
valentine <vmadu@gammax.exchange>
2023-10-30 07:38:55 +0100

    wip

--- stacks-and-queues.py
+++ stacks-and-queues.py
@@ -1,1 +1,33 @@
-# still implementing
+# stack with array
+class ArrayStack:
+    def __init__(self):
+        self.arr = []
+        self.length = 0
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def peek(self):
+        if self.length < 1:
+            print(f'No item in list')
+            return
+        print(self.arr[self.length-1])
+        return
+
+    def push(self, value):
+        self.arr.append(value)
+        self.length += 1
+
+    def pop(self):
+        popped_item = self.arr[self.length-1]
+        del self.arr[self.length-1]
+        self.length -= 1
+        return popped_item
+
+mystack = ArrayStack()
+mystack.push(8)
+mystack.push(4)
+mystack.push(15)
+mystack.pop()
+mystack.peek()
+print(mystack)
