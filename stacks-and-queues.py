fed986a47c607168341d66d6df7667655f7cdd5a
valentine <vmadu@gammax.exchange>
2023-10-30 08:42:14 +0100

    wip

--- stacks-and-queues.py
+++ stacks-and-queues.py
@@ -1,3 +1,7 @@
+from collections import deque
+import time
+import threading
+
 class Stack:
     def __init__(self):
         self.arr = []
@@ -30,3 +34,37 @@ class Stack:
 # mystack.pop()
 # mystack.peek()
 # print(mystack)
+
+# Queues
+class Queues:
+    def __init__(self):
+        self.buffer = deque()
+        self.length = 0
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def peek(self):
+        return self.buffer[self.length-1]
+    
+    def enqueue(self, data):
+        self.buffer.appendleft(data)
+        self.length += 1
+
+    def dequeue(self):
+        if self.length < 1:
+            print('queue is empty')
+            return
+        popped_item = self.buffer.pop() 
+        self.length -= 1
+        return popped_item
+
+myqueue = Queues()
+myqueue.enqueue(8)
+myqueue.enqueue(4)
+myqueue.enqueue(2)
+myqueue.enqueue(12)
+myqueue.enqueue(34)
+print(myqueue.peek()) 
+myqueue.dequeue()
+print(myqueue)
