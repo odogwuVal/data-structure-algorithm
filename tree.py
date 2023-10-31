370740dbe6e78946bf03d0c757c9b246d0832dc7
valentine <vmadu@gammax.exchange>
2023-10-31 06:11:44 +0100

    wip

---
+++ tree.py
@@ -0,0 +1,8 @@
+#  Binary tree
+# each node can only have eith zero, one or two nodes
+# each child can only have one parent
+# A node either has zero or two children but never one child 
+
+#  Binary search trees
+# All child nodes to the right of the root node must be greater than the parent node
+# All child nodes to the left of the root node must be less than the parent node
