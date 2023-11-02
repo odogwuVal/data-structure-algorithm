146845db829ddccf0bd181644da93c3a7c789167
valentine <vmadu@gammax.exchange>
2023-11-02 06:03:26 +0100

    wip

---
+++ recurssion.py
@@ -0,0 +1,36 @@
+# Understanding recursion
+# A recurssive function needs an allocation from the memory to remember the past calls.
+# this memory location is called a stack...the functions are stacked there till a base case is hit, 
+# and the content of the stack gets returned
+# In the absence of a base case, you get a stack overflow
+counter = 0
+def inception():
+    global counter
+    if counter > 6:
+        return 'done'
+    else:
+        counter += 1
+        return inception()
+        
+
+# inception()
+
+# Write a function that finds the factorial of any number.
+# Write the function recurssively and iteratively
+def factorialRecursion(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+factorialRecursion(5)
+
+def factorialIterative(n):
+    counter = 1
+    result = 1
+    while counter <= n:
+        result = result * counter
+        counter += 1
+    return result 
+
+factorialIterative(5)
