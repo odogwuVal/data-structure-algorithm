de0cea2290cb5f9112322d97f52bf652f9ca0817
valentine <vmadu@gammax.exchange>
2023-11-03 08:32:37 +0100

    start implementing  sorting

---
+++ sorting.py
@@ -0,0 +1,31 @@
+# Bubble sort
+# this is the least efficient
+def bubbleSort(arr):
+    a = 0
+    # this will make sure that we sort the entire length of the array
+    while a < len(arr):
+        # this checks for the two elements side by side and swaps when needed
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i] # replacement in python
+        a += 1
+    return arr
+
+unsorted_list = [6, 5, 3, 1, 8, 7, 2, 4]
+# print(bubbleSort(unsorted_list))
+
+# selection sort
+def selectionSort(arr):
+    a = 0
+    while a < len(arr):
+        smallest_number = arr[a]
+        index = a
+        for j in range(a+1, len(arr)):
+            if arr[j] < smallest_number:
+                index = j
+                smallest_number = arr[j]
+        arr[a], arr[index] = arr[index], arr[a]
+        a += 1
+    return arr
+
+# print(selectionSort(unsorted_list))
