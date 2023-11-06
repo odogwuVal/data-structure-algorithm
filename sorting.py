8f829f6c04a555e2766eea9de20f825c7943d59c
valentine <vmadu@gammax.exchange>
2023-11-06 05:37:12 +0100

    wip

--- sorting.py
+++ sorting.py
@@ -29,3 +29,21 @@ def selectionSort(arr):
     return arr
 
 # print(selectionSort(unsorted_list))
+
+# Insertion sort
+def insertionSort(arr):
+    length = len(arr)
+    i = 1
+    end = arr[0]
+    while i < length:
+        if arr[i] < end:
+            x = arr.pop(i)
+            for j in range(0, i):
+                if x < arr[j]:
+                    arr.insert(j, x)
+                    break
+        end = arr[i]
+        i += 1
+    return arr
+
+insertionSort(arr)
